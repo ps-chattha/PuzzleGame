c9bde4d51b46560817565d876b61c7661d56b4e6
ps-chattha <55334254+ps-chattha@users.noreply.github.com>
2020-02-11 22:55:19 -0500

    Update puzzle.java

--- puzzle.java
+++ puzzle.java
@@ -4,22 +4,19 @@ import java.awt.*;
 import javax.swing.*;  
 import java.awt.event.*;  
 public class puzzle extends JFrame implements ActionListener{  
-/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,n;  
 	puzzle(){  
 		super("Puzzle Game - JavaTpoint");  
 		b1=new JButton("1");  
-		b2=new JButton(" ");  
+		b9=new JButton("2");  
 		b3=new JButton("3");  
 		b4=new JButton("4");  
 		b5=new JButton("5");  
 		b6=new JButton("6");  
 		b7=new JButton("7");  
 		b8=new JButton("8");  
-		b9=new JButton("2");  
+		b2=new JButton(" ");  
 		n=new JButton("RESET");  
   
 		b1.setBounds(10,30,50,40);  
@@ -51,8 +48,7 @@ public class puzzle extends JFrame implements ActionListener{
 		setLayout(null);  
 		setVisible(true);  
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-	}//end of constructor  
-  
+	}
 	@SuppressWarnings("deprecation")
 	public void actionPerformed(ActionEvent e){  
 		if(e.getSource()==n){  
@@ -71,9 +67,7 @@ public class puzzle extends JFrame implements ActionListener{
 			String s=b1.getLabel();  
 			if(b2.getLabel().equals(" ")){ b2.setLabel(s); b1.setLabel(" ");}  
 			else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b1.setLabel(" ");}  
-		}//end of if  
-  
-		if(e.getSource()==b3){  
+		}if(e.getSource()==b3){  
 			String s=b3.getLabel();  
 			if(b2.getLabel().equals(" ")){ b2.setLabel(s); b3.setLabel(" ");}  
 			else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b3.setLabel(" ");}  
@@ -84,51 +78,38 @@ public class puzzle extends JFrame implements ActionListener{
 			if(b1.getLabel().equals(" ")){ b1.setLabel(s); b2.setLabel(" ");}  
 			else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b2.setLabel(" ");}  
 			else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b2.setLabel(" ");}  
-		}//end of if  
-  
-		if(e.getSource()==b4){  
+		}if(e.getSource()==b4){  
 			String s=b4.getLabel();  
 			if(b1.getLabel().equals(" ")){ b1.setLabel(s); b4.setLabel(" ");}  
 			else if(b7.getLabel().equals(" ")){ b7.setLabel(s); b4.setLabel(" ");}  
 			else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b4.setLabel(" ");}  
-		}//end of if  
-  
-		if(e.getSource()==b5){  
+		}if(e.getSource()==b5){  
 			String s=b5.getLabel();  
 			if(b2.getLabel().equals(" ")){ b2.setLabel(s); b5.setLabel(" ");}  
 			else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b5.setLabel(" ");}  
 			else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b5.setLabel(" ");}  
 			else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b5.setLabel(" ");}  
-		}//end of if  
-  
-		if(e.getSource()==b6){  
+		}if(e.getSource()==b6){  
   
 			String s=b6.getLabel();  
 			if(b9.getLabel().equals(" ")){ b9.setLabel(s); b6.setLabel(" ");}  
 			else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b6.setLabel(" ");}  
 			else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b6.setLabel(" ");}  
   
-		}//end of if  
-  
-		if(e.getSource()==b7){  
+		}if(e.getSource()==b7){  
 			String s=b7.getLabel();  
 			if(b4.getLabel().equals(" ")){ b4.setLabel(s); b7.setLabel(" ");}  
 			else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b7.setLabel(" ");}  
-		}//end of if  
-  
-		if(e.getSource()==b8){  
+		}if(e.getSource()==b8){  
 			String s=b8.getLabel();  
 			if(b7.getLabel().equals(" ")){ b7.setLabel(s); b8.setLabel(" ");}  
 			else if(b9.getLabel().equals(" ")){ b9.setLabel(s); b8.setLabel(" ");}  
 			else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b8.setLabel(" ");}  
-		}
-  
-		if(e.getSource()==b9){  
+		}if(e.getSource()==b9){  
 			String s=b9.getLabel();  
 			if(b6.getLabel().equals(" ")){ b6.setLabel(s); b9.setLabel(" ");}  
 			else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b9.setLabel(" ");}  
-  
-			if(b1.getLabel().equals("1")&&b2.getLabel().equals("2")&&b3.getLabel().equals("3")&&b4.getLabel().equals("4")&&b5.getLabel().equals("5")&&b6.getLabel().equals("6")&&b7.getLabel().equals("7")&&b8.getLabel().equals("8")&&b9.getLabel().equals(" ")){   
+  				if(b1.getLabel().equals("1")&&b2.getLabel().equals("2")&&b3.getLabel().equals("3")&&b4.getLabel().equals("4")&&b5.getLabel().equals("5")&&b6.getLabel().equals("6")&&b7.getLabel().equals("7")&&b8.getLabel().equals("8")&&b9.getLabel().equals(" ")){   
 				JOptionPane.showMessageDialog(puzzle.this,"!!!you won!!!");  
 			}  
 		}
